221cf9152274381f9863d5df9ef41aecb9950e7d
ChenGang <cgangEE@gmail.com>
2016-01-25 15:41:11 +0800

    .

--- dataClean.cpp
+++ dataClean.cpp
@@ -1,6 +1,5 @@
 #include "faceTool.h"
 
-#define picDirName  "origin_pic"
 #define DELTA 5
 #define SIZE 50
 #define SCALE 0.9
@@ -11,38 +10,55 @@ string createPicName(int no){
 		ret = (char)('0'+no%10) + ret;
 		no /= 10;
 	}
-	return "../result_pic/"+ret + ".jpg";
+	return ret + ".jpg";
 }
 
-void cutImage(Mat &img, int &fileCnt){
+int fileCnt;
+
+void processAndSave(Mat img, const char *dstDir){
+	Mat roi;
+	cvtColor(img, img, CV_BGR2GRAY);
+	double scale = max((SIZE+1.0)/img.rows, (SIZE+1.0)/img.cols);
+	Size size(img.cols * scale, img.rows * scale);
+	resize(img, img, size);
+	Rect rect(0, 8, SIZE, SIZE);
+	img(rect).copyTo(roi);
+	imwrite( (string(dstDir) + '/' + createPicName(++fileCnt)).c_str(), roi);  
+}
+
+void cutImage(Mat &img, const char* dstDir){
 	Mat roi;
 	cvtColor(img,img,CV_BGR2GRAY);
+
 	while (img.rows>=SIZE && img.cols>=SIZE){
 		for (int i=0; i<(img.cols- SIZE) / DELTA; ++i)
 			for (int j=0; j<(img.rows- SIZE) / DELTA; ++j){
-				if (rand()%100000<99900) continue;
 				Rect rect(i*DELTA, j*DELTA, SIZE, SIZE);
 				img(rect).copyTo(roi);
-				++fileCnt;
-				imwrite(createPicName(fileCnt).c_str(), roi);  
+				//imwrite(createPicName(fileCnt).c_str(), roi);  
+				imshow("x", roi);  
+				waitKey(100);
 			}
 		Size size(img.cols * SCALE, img.rows * SCALE);
 		resize(img, img, size);
 	}
 }
 
+
 bool processPicsInDir(const char *srcDir, const char *dstDir){
 	vector<string> picName;
 	if (!getFileNameFromDir(srcDir, picName)) return false;
 	for (int i=0; i<picName.size(); ++i){
-
+		Mat src = imread(picName[i].c_str());
+		processAndSave(src, dstDir);
 	}
 	return true;
 }
 
 int main(){
 	srand(time(NULL));
-	if (!processPicsInDir("origin_pic", "negative_pic")) return 1;
+//	if (!processPicsInDir("origin_pic", "negative_pic")) return 1;
+	if (!processPicsInDir("yale", "positive_pic")) return 1;
 
 	return 0;
 }
